f83d6838b97ee92096888b2803621c3e5c764cda
Kel Cecil <kel.cecil@listhub.com>
2014-03-06 11:07:22 -0500

    Use loadpath for loading quote file

--- lib/chuck.rb
+++ lib/chuck.rb
@@ -1,7 +1,7 @@
 class Chuck
   def self.say
     chosen_line = nil
-    File.foreach("#{File.dirname(__FILE__)}/../resources/quotes").each_with_index do |line,number|
+    File.foreach("resources/quotes").each_with_index do |line,number|
       chosen_line = line if rand < 1.0 / (number + 1)
     end
     return chosen_line.delete("\n")
